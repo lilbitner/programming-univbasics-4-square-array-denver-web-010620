1c834e51a60e7c1ce551174770f1b0f49e0b304c
Lillian Bitner <lillian.bitner@gmail.com>
2020-01-07 21:01:23 +0000

    Automatically backed up by Learn

--- lib/square_array.rb
+++ lib/square_array.rb
@@ -4,6 +4,16 @@
 # while array[counter] do 
  # puts ((array[counter])**(array[counter])) 
  # counter += 1 
+#end
+#new_array
+#end 
+
+def square_array(array)
+  squared = []
+  count = 0
+  while count < array.size do
+    squared << (array[count] ** 2)
+    count = count + 1
+  end
+  squared
 end
-new_array
-end 
